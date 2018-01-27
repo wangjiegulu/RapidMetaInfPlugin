5fafa4851aa0622fdd071fd7786c5afc2085e92d
WangJie <tiantian.china.2@gmail.com>
2018-01-27 14:45:42 +0800

    nothing

--- app/src/main/java/com/wangjiegulu/rapidmetainfplugin/MainActivity.java
+++ app/src/main/java/com/wangjiegulu/rapidmetainfplugin/MainActivity.java
@@ -1,7 +1,9 @@
 package com.wangjiegulu.rapidmetainfplugin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wangjiegulu.rapidmetainfplugin.depmodule.DepModuleTest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DepModuleTest().print();
     }
 }
---
+++ depmodule/src/main/java/com/wangjiegulu/rapidmetainfplugin/depmodule/DepModuleTest.java
@@ -0,0 +1,16 @@
+package com.wangjiegulu.rapidmetainfplugin.depmodule;
+
+import android.util.Log;
+
+/**
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 26/01/2018.
+ */
+public class DepModuleTest {
+    private static final String TAG = DepModuleTest.class.getSimpleName();
+
+    public void print(){
+        Log.i(TAG, "DepModuleTest print...");
+    }
+}
